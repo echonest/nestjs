e76edce9c4dd2b2549fd6e2453e159a782806292
Reid Draper <reiddraper@gmail.com>
2011-03-10 23:45:04 -0500

    add query_string func, nest_get func, have a basic artist bio example working

--- src/nest.js
+++ src/nest.js
@@ -4,21 +4,74 @@
 // Wrap everything in the `nest` object, as to not clobber the
 // global namespace
 var nest = {
+    // return a query string from an object
+    query_string: function(params) {
+        var query = '?';
+        var first = true;
+        for (key in params) {
+            if (params.hasOwnProperty(key)) {
+                if (first) {
+                    first = false;
+                } 
+                else {
+                    query+= '&';
+                }
+                query+= (key + '=' + params[key]);
+            }
+        }
+        return query;
+    },
+
     // This is the main object that is used
     // to call the api
     nest: function(api_key, host) {
         // optionaly take in another host,
         // for testing purposes
         host = host || "developer.echonest.com";
+        var api_path = "/api/v4/";
+
+        // make HTTP GET requests and call `callback` with the
+        // response object
+        function nest_get(category, method, query, callback) {
+            query.api_key = api_key;
+            query.format = 'json';
+            var request = new XMLHttpRequest();
+            var url = 'http://' + host + api_path + category + '/' + method + nest.query_string(query);
+            console.log('GET: ' + url);
+            request.open('GET', url, true);
+            request.onreadystatechange = function() {
+                if (request.readyState === 4) {
+                    if (request.status === 200 || request.status === 304) {
+                        var json_response = JSON.parse(request.responseText);
+                        callback(json_response);
+                    }
+                    else {
+                        callback({error: request.status});
+                    }
+                }
+            };
+            request.send();
+        }
         return {
             // return the read-only `api_key`
-            get_api_key: function(){
+            get_api_key: function() {
                 return api_key;
             },
             
             // return the read-only `host` name
-            get_host: function(){
+            get_host: function() {
                 return host;
+            },
+
+            // create a new artist
+            artist: function(id, name) {
+                var category = 'artist';
+                return {
+                    biographies: function(callback) {
+                        var query = {"id":id};
+                        return nest_get(category, 'biographies', query, callback);
+                    }
+                };
             }
         };
     }
