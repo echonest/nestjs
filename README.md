5942d9bdabe00828b6d4e075da3343110602ef11
Reid Draper <reiddraper@gmail.com>
2011-03-13 15:03:11 -0400

    better documentation, beginning support of artist methods

--- README.md
+++ README.md
@@ -1,3 +1,34 @@
 #nest.js 
-nest.js is a javascript wrapper to the Echo Nest 
-[developer api](http://developer.echonest.com).
+nest.js is a thin javascript wrapper to the Echo Nest 
+[developer API](http://developer.echonest.com).
+
+It is currently a work-in-progress.
+
+To get started, [you'll need to get an API key](http://developer.echonest.com/account/register).
+
+## Documentation
+In addition to this document, source documentation can be generated
+by running `generate_docs.sh`. This requires [docco](http://jashkenas.github.com/docco/).
+
+## Usage
+`nest.js` provides everything in a global `nest` object. We can create
+a new `nest` object like this
+
+`var myNest = nest.nest("your API key here");`
+
+Once you have your `nest` object, you can create a new `artist`. `artist` can
+be created with either a name, or an [Echo Nest ID](http://developer.echonest.com/docs/v4/index.html#identifiers).
+
+    var a = myNest.artist({name: "The Sea and Cake});
+    var b = myNest.artist({id: "AR94EZ61187B990729"});
+
+Once we have our artist object, we can start calling the API. All methods take a
+callback function as their last argument. The callback will be called like `callback(err, results)`. `err` will be null if the request was successful.
+
+    myArtist.biographies({results: 10, start: 5}, function(err, results) {
+        if (err) {
+            console.log("there was an error...");
+            return;
+        }
+        console.log(results);
+    });
--- src/nest.js
+++ src/nest.js
@@ -3,74 +3,152 @@
 
 // Wrap everything in the `nest` object, as to not clobber the
 // global namespace
-var nest = {
-    // return a query string from an object
-    query_string: function(params) {
-        var query = '?';
-        var first = true;
-        for (key in params) {
-            if (params.hasOwnProperty(key)) {
-                if (first) {
-                    first = false;
-                } else {
-                    query+= '&';
-                }
-                query+= (encodeURI(key) + '=' + encodeURI(params[key]));
+var nest = (function(){
+    // Helper function for iterating through
+    // the keys in an object
+    function each(obj, func) {
+        for (key in obj) {
+            if (obj.hasOwnProperty(key)) {
+                func.call(obj, key);
             }
         }
+    }
+
+    // Update `obj` with all of the
+    // key/values of `source`, not
+    // following the prototype
+    // chain
+    function update(obj, source) {
+        each(source, function(key) {
+            obj[key] = source[key];
+        });
+        return obj;
+    }
+    // return a query string from an object
+    function queryString(params) {
+        var query = '?';
+        var first = true;
+        each(params, function(key){
+            // only prepend `&` when this
+            // isn't the first k/v pair
+            if (first) {
+                first = false;
+            } else {
+                query+= '&';
+            }
+            query+= (encodeURI(key) + '=' + encodeURI(params[key]));
+        });
         return query;
-    },
+    }
 
     // This is the main object that is used
     // to call the api
-    nest: function(api_key, host) {
-        // optionaly take in another host,
-        // for testing purposes
-        host = host || "developer.echonest.com";
-        var api_path = "/api/v4/";
+    return {
+        nest: function(api_key, host) {
+            // optionaly take in another host,
+            // for testing purposes
+            host = host || "developer.echonest.com";
+            var api_path = "/api/v4/";
 
-        // make HTTP GET requests and call `callback` with the
-        // response object
-        function nestGet(category, method, query, callback) {
-            query.api_key = api_key;
-            query.format = 'json';
-            var request = new XMLHttpRequest();
-            var url = 'http://' + host + api_path + category + '/' + method + nest.query_string(query);
-            console.log('GET: ' + url);
-            request.open('GET', url, true);
-            request.onreadystatechange = function() {
-                if (request.readyState === 4) {
-                    if (request.status === 200 || request.status === 304) {
-                        var json_response = JSON.parse(request.responseText);
-                        callback(json_response);
-                    } else {
-                        callback({error: request.status});
-                    }
-                }
-            };
-            request.send();
-        }
-        return {
-            // return the read-only `api_key`
-            getAPIKey: function() {
-                return api_key;
-            },
-            
-            // return the read-only `host` name
-            getHost: function() {
-                return host;
-            },
+            // make HTTP GET requests and call `callbacks.success`
+            // or `callbacks.error` with the
+            // response object, or the `status`, on error
+            function nestGet(category, method, query, callback) {
+                query.api_key = api_key;
+                query.format = 'json';
+                var request = new XMLHttpRequest();
+                var url = 'http://';
+                url+= host;
+                url+= api_path;
+                url+= category + '/';
+                url+= method;
+                url+= queryString(query);
 
-            // create a new artist
-            artist: function(id, name) {
-                var category = 'artist';
-                return {
-                    biographies: function(callback) {
-                        var query = {"id":id};
-                        return nestGet(category, 'biographies', query, callback);
+                request.open('GET', url, true);
+                request.onreadystatechange = function() {
+                    // see if the response is ready
+                    if (request.readyState === 4) {
+                        // get the request status class, ie.
+                        // 200 == 2, 404 == 4, etc.
+                        var sc = Math.floor(request.status / 100);
+                        if (sc === 2 || sc === 3) {
+                            var json_response = JSON.parse(request.responseText);
+                            // unwrap the response from the outter
+                            // `response` wrapper
+                            var response = json_response.response;
+                            callback(null, response);
+                        } else {
+                            // there was an error,
+                            // just return the `status`
+                            // as the first paramter
+                            callback(request.status);
+                        }
                     }
                 };
+                // do it
+                request.send();
             }
-        };
-    }
-};
+            return {
+                // return the read-only `api_key`
+                getAPIKey: function() {
+                    return api_key;
+                },
+                
+                // return the read-only `host` name
+                getHost: function() {
+                    return host;
+                },
+
+                // create a new artist
+                // params should have an
+                // `id` or `name` property
+                artist: function(params) {
+                    // the method category
+                    var category = 'artist';
+                    // we'll be attaching functions to this
+                    // object
+                    var artist = {};
+                    // enumerate the list of methods
+                    // we'll be attaching to `artist`
+                    var methods = ['audio',
+                    'biographies',
+                    'blog',
+                    'images',
+                    'news',
+                    'reviews',
+                    'songs',
+                    'similar',
+                    'video'];
+                    // helper function for having a closure remember
+                    // a value in when it changes in a loop
+                    function helper(method) {
+                        // this will be the function that
+                        // gets called for each of the methods
+                        // on `artist`. It can be called
+                        // with a variable number of arguments,
+                        // the last one always being a callback function.
+                        // The callback function will be called like
+                        // `callback(err, result)`
+                        return function() {
+                            var args = Array.prototype.slice.call(arguments);
+                            var callback = args.pop();
+                            var query = update({}, params);
+                            var options = args.pop();
+                            if (options) {
+                                update(query, options);
+                            }
+                            return nestGet(category, method, query, callback);
+                        };
+                    }
+                    for (i = 0; i < methods.length; i++) {
+                        // go through and attach a function
+                        // to each of the `artist` methods
+                        var method = methods[i];
+                        artist[method] = helper(method);
+                    }
+                    return artist;
+                }
+            };
+        }
+    };
+})();
